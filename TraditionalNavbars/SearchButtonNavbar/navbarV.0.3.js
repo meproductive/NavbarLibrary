86a62a5ff74cca909f050f66f57fcf592f66dea3
meproductive <me.productive0@gmail.com>
2025-02-08 00:03:56 +0300

    new code lines added

--- TraditionalNavbars/SearchButtonNavbar/navbarV.0.3.js
+++ TraditionalNavbars/SearchButtonNavbar/navbarV.0.3.js
@@ -2,11 +2,11 @@
 const navigation = document.getElementById("navigation");
 const menubtn = document.getElementById("menu-btn");
 
-    //Function Declaration ES6+
-    const openFunc = () => {
+    //Function declaration ES6+
+    const hamburgerMenu = () => {
         navigation.classList.toggle('active');
         menubtn.classList.toggle('active');
     };
 
   //EVENTLISTENER on click function 
-  menubtn.addEventListener('click', openFunc);
+  menubtn.addEventListener('click', hamburgerMenu);
