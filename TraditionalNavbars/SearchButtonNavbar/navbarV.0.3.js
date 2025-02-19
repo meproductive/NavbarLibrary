c15871a2dde6f71f8980b30f2ed2869730c75579
meproductive <me.productive0@gmail.com>
2025-02-19 19:27:05 +0300

    Javascript updated

--- TraditionalNavbars/SearchButtonNavbar/navbarV.0.3.js
+++ TraditionalNavbars/SearchButtonNavbar/navbarV.0.3.js
@@ -1,30 +1,38 @@
-//Button and Navigation's dom declarations
+// Button and Navigation's dom declarations
 const navigation = document.getElementById('navigation');
 const menubtn = document.getElementById('menu-btn');
 
-    //Function declaration ES6+
+    // Function declaration ES6+
     const hamburgerMenu = () => {
         navigation.classList.toggle('active');
         menubtn.classList.toggle('active');
     };
 
-  //EVENTLISTENER on click function 
+  // EVENTLISTENER on click function
   menubtn.addEventListener('click', hamburgerMenu);
 
- //Buttons's & input's dom declarations
+ // Buttons's & input's dom declarations
  const searchbtn = document.getElementById('search-btn');
  const closebtn = document.getElementById('close-btn');
  const searchinput = document.getElementById('search-input');
+
+ // ul tag's parent id's dom declaration
  const parentt = document.getElementById('parent').children;
+ // matched media screen size min-width 768px passes to variable
+ const primaryItems = window.matchMedia('(min-width: 768px)');
 
     const openSearchinput = () => {
 
         searchinput.classList.add('active');
         searchbtn.classList.add('active');
         closebtn.classList.add('active');
-
-        for (let i = 0; i < 4; i++){
-            parentt[i].style.display = 'none';
+        
+        // if statement checks if primaryItem's value matches
+        if (primaryItems.matches) {
+            // for loop iterates until fourths child of parent element and applies css styling display to each child
+            for (let i = 0; i < 4; i++){
+                parentt[i].style.display = 'none';
+            }
         }
 
     };
@@ -35,11 +43,13 @@ const menubtn = document.getElementById('menu-btn');
         searchinput.classList.remove('active');
         searchbtn.classList.remove('active');
 
+        // for loop iterates until fourths child of parent element and applies css styling display to each child
         for (let i = 0; i < 4; i++){
             parentt[i].style.display = 'flex';
         }
         
     }
 
+    // EVENTLISTENER on click functions
     searchbtn.addEventListener('click', openSearchinput);
     closebtn.addEventListener('click', closeSearchinput);
